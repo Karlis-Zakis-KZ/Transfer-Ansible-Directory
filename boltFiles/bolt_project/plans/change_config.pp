c1f6aa1f5cbb14a47fa5816bb5325f736efba3ad
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-19 18:41:06 +0300

    Update 30

--- boltFiles/bolt_project/plans/change_config.pp
+++ boltFiles/bolt_project/plans/change_config.pp
@@ -9,12 +9,12 @@ plan bolt_project::change_config(
 
   $targets.each |$target| {
     # Gather current running config
-    $result = run_task('cisco_ios::command', $target, { 'commands' => $cmds })
-    $running_config = $result[0]['result']['result']
+    $result = run_task('cisco_ios::cli_command', $target, { 'commands' => $cmds })
+    $running_config = $result[0]['result']['stdout']
 
     # Check if the ACL configuration is already present
     if !($running_config =~ /ip access-list standard TEST_ACL/ and $running_config =~ /permit 192.168.0.0 0.0.255.255/) {
-      run_task('cisco_ios::command', $target, { 'commands' => $acl_lines })
+      run_task('cisco_ios::cli_command', $target, { 'commands' => $acl_lines })
     } else {
       out::message("Configuration already present on ${target.uri}")
     }
