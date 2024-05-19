8c44823b556bebe6b18eaa22e1eef6853e5a3d08
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-19 18:37:11 +0300

    Update 28

--- boltFiles/bolt_project/plans/change_config.pp
+++ boltFiles/bolt_project/plans/change_config.pp
@@ -1,34 +1,22 @@
-# bolt_project/plans/change_config.pp
 plan bolt_project::change_config(
   TargetSpec $targets
 ) {
-  $cmd = 'show running-config'
+  $cmds = ['show running-config']
   $acl_lines = [
     'ip access-list standard TEST_ACL',
     'permit 192.168.0.0 0.0.255.255'
   ]
 
-  $all_targets = get_targets($targets)
+  $targets.each |$target| {
+    # Gather current running config
+    $result = run_task('cisco_ios::command', $target, { 'commands' => $cmds })
+    $running_config = $result[0]['result']['output']
 
-  $all_targets.each |$target| {
-    out::message("Running command on ${target.name}")
-
-    $result = run_command($cmd, $target)
-    if $result.error {
-      fail_plan("Failed to run command on ${target.name}: ${result.error.message}")
-    }
-    $running_config = $result[0]['stdout']
-
+    # Check if the ACL configuration is already present
     if !($running_config =~ /ip access-list standard TEST_ACL/ and $running_config =~ /permit 192.168.0.0 0.0.255.255/) {
-      $acl_lines.each |$line| {
-        out::message("Applying ACL on ${target.name}: ${line}")
-        $acl_result = run_command($line, $target)
-        if $acl_result.error {
-          fail_plan("Failed to apply ACL on ${target.name}: ${acl_result.error.message}")
-        }
-      }
+      run_task('cisco_ios::command', $target, { 'commands' => $acl_lines })
     } else {
-      out::message("Configuration already present on ${target.name}")
+      out::message("Configuration already present on ${target.uri}")
     }
   }
 }
