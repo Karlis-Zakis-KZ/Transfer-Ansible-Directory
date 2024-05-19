43a360bbf061acccdab7df4659714f03d7c1e780
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-19 12:58:20 +0300

    Update 16

--- boltFiles/change_config.pp
+++ boltFiles/change_config.pp
@@ -1,12 +1,21 @@
-plan change_config(
+# change_config.pp
+plan bolt_project::change_config(
   TargetSpec $targets
 ) {
-  $config_command = @("END"
-    ip access-list standard TEST_ACL
-    permit 192.168.0.0 0.0.255.255
-  END
+  $cmds = ['show running-config']
+  $acl_lines = [
+    'ip access-list standard TEST_ACL',
+    'permit 192.168.0.0 0.0.255.255'
+  ]
 
   $targets.each |$target| {
-    run_command($config_command, $target)
+    $result = run_command($cmds, $target)
+    $running_config = $result[0]['stdout']
+    
+    if !($running_config =~ /ip access-list standard TEST_ACL/ and $running_config =~ /permit 192.168.0.0 0.0.255.255/) {
+      run_command($acl_lines, $target)
+    } else {
+      out::message("Configuration already present on ${target.uri}")
+    }
   }
 }
