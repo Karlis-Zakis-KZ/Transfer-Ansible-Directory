f3be098c981bc741af99bac1d8e7bfad3f0bfb6f
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-19 20:26:24 +0300

    Update 52

--- boltFiles/tasks/ios_command.sh
+++ boltFiles/tasks/ios_command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-command=$1
 
-sshpass -p "$PT_password" ssh -o StrictHostKeyChecking=no $PT_user@$PT_host "$command"
+# Script to run IOS command via SSH
+sshpass -p "$PT_password" ssh -o StrictHostKeyChecking=no "$PT_user@$PT_host" "$PT_command"
