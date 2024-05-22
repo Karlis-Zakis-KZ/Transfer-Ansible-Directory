9d92fb37c75a2d5b27cf9d02118a1db729517f79
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-22 15:11:43 +0300

    Update 95

--- boltFiles/bolt_project/tasks/network_config_task.sh
+++ boltFiles/bolt_project/tasks/network_config_task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source /path/to/your/venv/bin/activate
+source /home/osboxes/Transfer-Ansible-Directory/boltFiles/bolt_project/venv/bin/activate
 
 # Run the Python script with the provided parameters
-python ~/Transfer-Ansible-Directory/boltFiles/bolt_project/tasks/network_config_with_capture.py "$PT_interface" "$PT_runs" "~/Transfer-Ansible-Directory/boltFiles/bolt_project/hosts.txt"
+python /home/osboxes/Transfer-Ansible-Directory/boltFiles/bolt_project/network_config_with_capture.py "$PT_interface" "$PT_runs" "/home/osboxes/Transfer-Ansible-Directory/boltFiles/bolt_project/hosts.txt"
