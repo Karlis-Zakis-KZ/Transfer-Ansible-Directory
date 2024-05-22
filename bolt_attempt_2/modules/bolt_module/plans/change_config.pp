174accee89ab84532374dadc17d1f5f9008c862b
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-22 19:51:49 +0300

    156

--- bolt_attempt_2/modules/bolt_module/plans/change_config.pp
+++ bolt_attempt_2/modules/bolt_module/plans/change_config.pp
@@ -6,25 +6,21 @@ plan bolt_module::change_config(
 ) {
   $acl_command = "access-list ${acl_name} permit ip ${ip_range} ${wildcard_mask}"
 
-  $targets.each |$target| {
-    out::message("Applying ACL command to ${target.uri}")
+  out::message("Target string: ${targets}")
+
+  $target_array = $targets.split(',')
+
+  $target_array.each |$target| {
+    out::message("Applying ACL command to ${target}")
 
     # Run the ACL command on the target machine
-    $commands = [
-      "configure terminal",
-      $acl_command,
-      "end",
-      "write memory"
-    ]
+    run_command("configure terminal", $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    run_command($acl_command, $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    run_command("exit", $target, '_run_as' => 'karlis', 'password' => 'cisco')
 
-    $commands.each |$cmd| {
-      $output = run_command($cmd, $target)
-      out::message("Output for command '${cmd}': ${$output['stdout']}")
-    }
+    out::message("Verifying ACL on ${target}")
 
-    out::message("Verifying ACL on ${target.uri}")
-
-    $output = run_command("show access-lists ${acl_name}", $target)
-    out::message($output['stdout'])
+    $output = run_command("show access-lists ${acl_name}", $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    out::message($output.stringify)
   }
 }
