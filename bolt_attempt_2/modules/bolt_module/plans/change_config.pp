e3a0a1f7e814dfa651edca10f7357df1ecf46708
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-23 00:56:11 +0300

    Temp Test 5

--- bolt_attempt_2/modules/bolt_module/plans/change_config.pp
+++ bolt_attempt_2/modules/bolt_module/plans/change_config.pp
@@ -14,42 +14,18 @@ plan bolt_module::change_config(
   $target_array.each |$target| {
     out::message("Applying ACL command to ${target}")
 
-    # Run the enable command on the target machine
-    $enable_output = run_command("enable", $target)
-    if $enable_output['exit_code'] != 0 {
-      fail("Error running enable on ${target}: ${enable_output['stderr']}")
-    }
+    # Run the ACL command on the target machine
+    run_command("enable", $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    run_command("configure terminal", $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    $whatResutns = run_command($acl_command, $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    out::message($whatResutns)
+    run_command($acl_command_permit, $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    run_command("end", $target, '_run_as' => 'karlis', 'password' => 'cisco')
 
-    # Run the configure terminal command
-    $configure_output = run_command("configure terminal", $target)
-    if $configure_output['exit_code'] != 0 {
-      fail("Error running configure terminal on ${target}: ${configure_output['stderr']}")
-    }
-
-    # Apply the ACL command
-    $acl_output = run_command($acl_command, $target)
-    if $acl_output['exit_code'] != 0 {
-      fail("Error applying ACL command on ${target}: ${acl_output['stderr']}")
-    }
-    out::message("ACL command output: ${acl_output['stdout']}")
-
-    # Apply the permit command within the ACL context
-    $permit_output = run_command($acl_command_permit, $target)
-    if $permit_output['exit_code'] != 0 {
-      fail("Error applying permit command on ${target}: ${permit_output['stderr']}")
-    }
-    out::message("Permit command output: ${permit_output['stdout']}")
-
-    # Exit configuration mode
-    $end_output = run_command("end", $target)
-    if $end_output['exit_code'] != 0 {
-      fail("Error running end on ${target}: ${end_output['stderr']}")
-    }
-
-    # Verify the ACL
     out::message("Verifying ACL on ${target}")
-    $output = run_command("show access-lists ${acl_name}", $target)
 
+    $output = run_command("show access-lists ${acl_name}", $target, '_run_as' => 'karlis', 'password' => 'cisco')
+    
     if $output['exit_code'] == 0 {
       $stdout = $output['stdout']
       out::message($stdout)
