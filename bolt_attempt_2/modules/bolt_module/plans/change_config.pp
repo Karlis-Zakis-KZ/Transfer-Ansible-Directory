b1122f2a6756a842b4a07758e91b4eb7407ca8f6
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-22 16:57:21 +0300

    105

--- bolt_attempt_2/modules/bolt_module/plans/change_config.pp
+++ bolt_attempt_2/modules/bolt_module/plans/change_config.pp
@@ -7,22 +7,27 @@ plan bolt_module::change_config(
 ) {
   # Start tcpdump
   $tcpdump_result = run_command('sudo tcpdump -i ens33 -w /tmp/tcpdump_output.pcap', $targets, '_run_as' => 'root')
-  
+
+  # Prepare the ACL configuration commands
+  $acl_commands = @("END")
+    configure terminal
+    access-list ${acl_name} permit ip ${ip_range} ${wildcard_mask}
+    end
+  END
+
   # Apply the ACL configuration on the routers
-  run_command("echo 'access-list ${acl_name} permit ip ${ip_range} ${wildcard_mask}' > /tmp/acl_config.txt", $targets)
-  run_command("sshpass -p 'cisco' ssh -o StrictHostKeyChecking=no karlis@192.168.21.11 'configure terminal' < /tmp/acl_config.txt", $targets)
-  run_command("sshpass -p 'cisco' ssh -o StrictHostKeyChecking=no karlis@192.168.21.12 'configure terminal' < /tmp/acl_config.txt", $targets)
-  run_command("sshpass -p 'cisco' ssh -o StrictHostKeyChecking=no karlis@192.168.21.13 'configure terminal' < /tmp/acl_config.txt", $targets)
-  run_command("sshpass -p 'cisco' ssh -o StrictHostKeyChecking=no karlis@192.168.21.14 'configure terminal' < /tmp/acl_config.txt", $targets)
+  $targets.each |$target| {
+    run_command("sshpass -p 'cisco' ssh -o StrictHostKeyChecking=no karlis@${target.uri} '${acl_commands}'", $target)
+  }
 
   # Stop tcpdump
   run_command('sudo pkill -f tcpdump', $targets, '_run_as' => 'root')
 
   # Analyze the pcap file to count packets
   $packet_count = run_command('capinfos /tmp/tcpdump_output.pcap | grep "Number of packets"', $targets)
-  
+
   # Verify the ACL configuration on the routers
-  $acl_verification = run_command("sshpass -p 'cisco' ssh -o StrictHostKeyChecking=no karlis@192.168.21.11 'show access-lists ${acl_name}'", $targets)
+  $acl_verification = run_command("sshpass -p 'cisco' ssh -o StrictHostKeyChecking=no karlis@${targets[0].uri} 'show access-lists ${acl_name}'", $targets)
   out::message("ACL Configuration:\n${acl_verification}")
 
   # Return packet count and ACL verification results
