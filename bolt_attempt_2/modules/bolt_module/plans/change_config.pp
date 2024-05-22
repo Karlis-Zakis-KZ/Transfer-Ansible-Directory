0e15a8c34249d4ec6f24cf68585257776091843d
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-22 19:34:34 +0300

    149

--- bolt_attempt_2/modules/bolt_module/plans/change_config.pp
+++ bolt_attempt_2/modules/bolt_module/plans/change_config.pp
@@ -11,17 +11,16 @@ plan bolt_module::change_config(
   $target_array = $targets.split(',')
 
   $target_array.each |$target| {
-    out::message("Target acl_name: ${acl_name}")
     out::message("Applying ACL command to ${target}")
 
     # Run the ACL command on the target machine
-    run_command("configure terminal", $target, '_run_as' => 'root')
-    run_command($acl_command, $target, '_run_as' => 'root')
-    run_command("exit", $target, '_run_as' => 'root')
+    run_command("configure terminal", $target, '_run_as' => 'karlis')
+    run_command($acl_command, $target, '_run_as' => 'karlis')
+    run_command("exit", $target, '_run_as' => 'karlis')
 
     out::message("Verifying ACL on ${target}")
 
-    $output = run_command("show access-lists ${acl_name}", $target, '_run_as' => 'root')
+    $output = run_command("show access-lists ${acl_name}", $target, '_run_as' => 'karlis')
     out::message($output['stdout'])
   }
 }
