fcd2323e541dffa085c136219ed109ea130c7c92
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-25 00:06:43 +0300

    bolt complex 13

--- puppet_bolt_complex/tasks/test_ssh.sh
+++ puppet_bolt_complex/tasks/test_ssh.sh
@@ -5,6 +5,6 @@ set -e
 HOST=$PT_host
 
 # Simple SSH command to test connectivity
-/usr/bin/sshpass -p cisco ssh -o StrictHostKeyChecking=no karlis@${HOST} "echo 'SSH Connection Successful'"
+ssh -o StrictHostKeyChecking=no karlis@${HOST} "echo 'SSH Connection Successful'"
 
 echo "SSH Connection Test Completed."
