19aadece89207f6c6de34f96d68265b55577bece
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-26 15:49:27 +0300

    temp

--- aplly_config_puppetredo/tasks/command.sh
+++ aplly_config_puppetredo/tasks/command.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-command=$PT_command
-sshpass -p "$PT_password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 "$PT_user@$PT_target" "$command"
+#!/bin/sh
+for cmd in "$commands"; do
+  vtysh -c "$cmd"
+done
--- aplly_config_puppetredo/tasks/config.sh
+++ aplly_config_puppetredo/tasks/config.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-config=$PT_config
-sshpass -p "$PT_password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 "$PT_user@$PT_target" << EOF
-enable
-conf t
-$config
-end
-write memory
-EOF
+#!/bin/sh
+for parent in "$parents"; do
+  vtysh -c "$parent"
+done
+
+for line in "$lines"; do
+  vtysh -c "$line"
+done
